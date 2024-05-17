04ddf183113ed64368a0952decb03ddf36f3de89
devXyz <ranesoham301@gmail.com>
2024-05-17 20:53:48 +0530

    Changed navbar design

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { IoLogOutOutline } from 'react-icons/io5';
+import { IoPersonOutline,IoLogOutOutline } from 'react-icons/io5';
 import './css/Navbar.css';
 import successIcon from "./assets/icons8-success-16.png";
 import errorIcon from "./assets/icons8-error-32.png";
@@ -50,6 +50,12 @@ const Navbar = ({ connection }) => {
         getProfile();
     }, []);
 
+    const [showMenu, setShowMenu] = useState(false);
+
+    const toggleMenu = () => {
+        setShowMenu(!showMenu);
+    };
+
     return (
         <nav className="navbar">
             <div className="navbar-logo px-5">
@@ -65,14 +71,29 @@ const Navbar = ({ connection }) => {
                 </Tooltip>
                 }
             </div>
-            {localStorage.getItem('user') && username!=='' &&
+            {localStorage.getItem('user') && connection && username!=='' &&
                 <div className="navbar-user">
-                    <span className="navbar-username btn mx-4" onClick={() => window.location.href = '/profile'}>
-                        {username}
+                    <button
+                        style={{ backgroundColor: '#2563EB', color: '#FFFFFF', padding: '0.5rem 1rem', borderRadius: '0.375rem', transition: 'background-color 0.3s' }}
+                        className="btn mx-4 hover:bg-blue-600"
+                        onClick={() => (window.location.href = '/profile')}
+                    >
+                        Dashboard
+                    </button>
+                    <span className="navbar-logout-icon btn rounded-full" onClick={toggleMenu}>
+                        <IoPersonOutline/>
                     </span>
-                    <span className="navbar-logout-icon btn rounded-full" onClick={handleLogout}>
-                        <IoLogOutOutline />
-                    </span>
+                    {showMenu && (
+                        <div className="absolute right-0 mx-3 mt-40 w-48 bg-white rounded-md shadow-lg z-10">
+                            <div className="px-4 py-2 border-b border-gray-300">
+                                <span className="block text-gray-700 text-center">{username}</span>
+                            </div>
+                            <div className="flex items-center px-4 py-2 cursor-pointer hover:bg-gray-100" onClick={handleLogout}>
+                                <IoLogOutOutline className="mx-2" />
+                                <span>Sign out</span>
+                            </div>
+                        </div>
+                    )}
                 </div>
             }
         </nav>
